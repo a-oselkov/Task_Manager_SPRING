0cddf4e722facffebcc98629d5fcfa3993527601
a-oselkov <a.oselkov@gmail.com>
2023-08-09 22:35:42 +0300

    disable  rollbar

--- src/main/java/hexlet/code/controller/WelcomeController.java
+++ src/main/java/hexlet/code/controller/WelcomeController.java
@@ -1,18 +1,16 @@
 package hexlet.code.controller;
 
-import com.rollbar.notifier.Rollbar;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
-    @Autowired
-    private Rollbar rollbar;
+//    @Autowired
+//    private Rollbar rollbar;
 
     @GetMapping("/welcome")
     public String welcome() {
-        rollbar.debug("Test message 5");
+        //rollbar.debug("Test message 5");
         return "welcome";
     }
 }
