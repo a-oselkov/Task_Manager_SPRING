d02ea53002db5999dc2a3a2609026335122918ca
a-oselkov <a.oselkov@gmail.com>
2023-08-28 20:10:07 +0300

    update import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,12 @@
--- INSERT INTO users (created_at, first_name, last_name, email, password) VALUES ('2023-01-01 00:00:00', 'Иван', 'Иванов', 'test@user.com', '$2a$10$GoQd22V6y0Gk6EoPjaO3xeKs1KFFfHxHtrkAUZfF4JB6Lk.f7FIrq');
+INSERT INTO users (created_at, first_name, last_name, email, password) VALUES ('2023-01-01 00:00:00', 'Иван', 'Иванов', 'test@user.com', '$2a$10$GoQd22V6y0Gk6EoPjaO3xeKs1KFFfHxHtrkAUZfF4JB6Lk.f7FIrq');
 
--- INSERT INTO task_statuses (created_at, name) VALUES ('2023-01-01 00:00:00', 'Новая');
--- INSERT INTO task_statuses (created_at, name) VALUES ('2023-01-01 00:00:00', 'В работе');
--- INSERT INTO task_statuses (created_at, name) VALUES ('2023-01-01 00:00:00', 'Завершена');
+INSERT INTO task_statuses (created_at, name) VALUES ('2023-01-01 00:00:00', 'Новая');
+INSERT INTO task_statuses (created_at, name) VALUES ('2023-01-01 00:00:00', 'В работе');
+INSERT INTO task_statuses (created_at, name) VALUES ('2023-01-01 00:00:00', 'Завершена');
 
--- INSERT INTO labels (created_at, name) VALUES ('2022-10-10 00:00:00', 'Frontend');
--- INSERT INTO labels (created_at, name) VALUES ('2022-10-10 00:00:00', 'Backend');
+INSERT INTO labels (created_at, name) VALUES ('2022-10-10 00:00:00', 'Frontend');
+INSERT INTO labels (created_at, name) VALUES ('2022-10-10 00:00:00', 'Backend');
 
--- INSERT INTO tasks (created_at, name, description, task_status_id, author_id, executor_id) VALUES ('2022-10-10 00:00:00', 'Проект №5', 'Внести изменения', 1, 1, 1);
+INSERT INTO tasks (created_at, name, description, task_status_id, author_id, executor_id) VALUES ('2022-10-10 00:00:00', 'Проект №5', 'Внести изменения', 1, 1, 1);
 
--- INSERT INTO task_label (task_id, label_id) VALUES (1, 1), (1,2);
+INSERT INTO task_label (task_id, label_id) VALUES (1, 1), (1,2);
